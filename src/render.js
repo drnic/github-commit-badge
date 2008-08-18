d050455e1900ccda6a521c187b5267cef9abf14b
Dr Nic Williams <drnicwilliams@gmail.com>
2008-08-18 19:27:59 +1000

    first start at rendering badge using unit tests

---
+++ src/render.js
@@ -0,0 +1,22 @@
+var GitHub = GitHub || {};
+GitHub.CommitBadge = GitHub.CommitBadge || {};
+
+GitHub.CommitBadge.loadData = function(commitMessages, targetElement, limit) {
+  targetElement.addClass('github-commit-badge-container');
+  var template = $.template(
+    '<div class="item github-commit-badge-outline">' +
+      '<a href="${projectUrl}" class="github-commit-badge-username">${projectName}</a>' +
+      '<img src="${gravatar}" class="github-commit-badge-gravatar">' +
+    '</div>');
+
+  for (var i=0; i < commitMessages.commits.length && i < limit; i++) {
+    var commit = commitMessages.commits[i];
+    var projectMatch = commit.url.match(/github.com\/([^\/]+)\/([^\/]+)\/commit\//);
+    targetElement.append(template, {
+      projectUrl: commit.url.match(/(.*)\/commit\//)[1],
+      committer: projectMatch[1],
+      projectName: projectMatch[2],
+      gravatar: "http://www.gravatar.com/avatar/" + hex_md5(commit.committer.email) + "?s=60"
+    });
+  };
+};
---
+++ test/fixtures/commit_messages.js
@@ -0,0 +1,40 @@
+var commitMessages = {
+    "commits": [{
+        "message": "GITHUB_LIST_LENGTH variable to edit length of list from 10 items",
+        "parents": [{
+            "id": "2de4bfe0fea5c04df07fab18c3daeb1cca93120d"
+        }],
+        "url": "http://github.com/drnic/github-badges/commit/69cf2d85e8173cdb2a5df46152136f62b5c2a96e",
+        "author": {
+            "name": "Dr Nic Williams",
+            "email": "drnicwilliams@gmail.com"
+        },
+        "id": "69cf2d85e8173cdb2a5df46152136f62b5c2a96e",
+        "committed_date": "2008-05-02T20:32:33-07:00",
+        "authored_date": "2008-05-02T20:32:33-07:00",
+        "tree": "964b0a21eb8500cde0dbaa01b4313de6912ab8ed",
+        "committer": {
+            "name": "Dr Nic Williams",
+            "email": "drnicwilliams@gmail.com"
+        }
+    },
+    {
+        "message": "network/watching values used first, then remainder in reverse order",
+        "parents": [{
+            "id": "d64b2072bfb32322dfac03cc4da67e703066a7f4"
+        }],
+        "url": "http://github.com/drnic/github-badges/commit/2de4bfe0fea5c04df07fab18c3daeb1cca93120d",
+        "author": {
+            "name": "Dr Nic Williams",
+            "email": "drnicwilliams@gmail.com"
+        },
+        "id": "2de4bfe0fea5c04df07fab18c3daeb1cca93120d",
+        "committed_date": "2008-05-02T20:23:35-07:00",
+        "authored_date": "2008-05-02T20:23:35-07:00",
+        "tree": "09e49d10c03f8e4d774f2514b2249cd6452153c2",
+        "committer": {
+            "name": "Dr Nic Williams",
+            "email": "drnicwilliams@gmail.com"
+        }
+    }]
+};
